6f0705e38a7ae7c611a58df35cd50012b21f69d5
René Sackers <rene.sackers@live.com>
2017-05-06 17:00:17 +0200

    Added all vehicle spawner.

---
+++ spawner/Client/main.js
@@ -0,0 +1,83 @@
+var menuPool = API.getMenuPool();
+var menus = [];
+
+// Main menu
+var mainMenu = API.createMenu("SPAWNER", 50, 50, 2)
+menus.push(mainMenu);
+
+API.onResourceStart.connect(function (sender, e) {
+    var groupedVehicles = [];
+
+    for (var i = 0; i < resource.vehicles.vehicleHashes.length; i++) {
+        var vehicleHash = resource.vehicles.vehicleHashes[i];
+        var vehicleClass = resource.vehicles.vehicleClassNames[API.getVehicleClass(vehicleHash)];
+
+        if (groupedVehicles[vehicleClass] == undefined) {
+            groupedVehicles[vehicleClass] = [];
+        }
+
+        groupedVehicles[vehicleClass].push({ hash: vehicleHash, name: API.getVehicleDisplayName(vehicleHash) });
+    }
+
+    for (var group in groupedVehicles) {
+        if (!groupedVehicles.hasOwnProperty(group)) continue;
+
+        var groupName = group;
+        var vehicles = groupedVehicles[group];
+        var categoryMenu = createVehicleCategory(groupName);
+
+        for (var i = 0; i < vehicles.length; i++) {
+            var vehicle = vehicles[i];
+            createSpawnVehicleItem(vehicle.name, vehicle.hash, categoryMenu);
+        }
+    }
+
+    // Add all menus to menu pool
+    for (var i = 0; i < menus.length; i++) {
+        menuPool.Add(menus[i]);
+    }
+});
+
+// Menu creation
+function createVehicleCategory(name) {
+    var vehicleCategoryMenu = API.createMenu(name, 50, 50, 2)
+    menus.push(vehicleCategoryMenu);
+
+    var vehicleCategoryItem = API.createMenuItem(name, "");
+
+    mainMenu.AddItem(vehicleCategoryItem);
+    mainMenu.BindMenuToItem(vehicleCategoryMenu, vehicleCategoryItem);
+
+    return vehicleCategoryMenu;
+}
+
+function createSpawnVehicleItem(name, hash, parentMenu) {
+    var menuItem = API.createMenuItem(name, "");
+    menuItem.Activated.connect(function (menu, item) {
+		API.triggerServerEvent("CREATE_VEHICLE", hash);
+    });
+    parentMenu.AddItem(menuItem);
+}
+
+// Menu handling
+API.onKeyDown.connect(function(sender, keyEventArgs) {
+	if (keyEventArgs.KeyCode == Keys.F2) {
+        // If any menus open, close them.
+        var anyOpen = false;
+        for (var i = 0; i < menus.length; i++) {
+            if (menus[i].Visible) {
+                anyOpen = true;
+                menus[i].Visible = false;
+            }
+        }
+
+        if (anyOpen) return;
+        // Otherwise, open main menu.
+
+		mainMenu.Visible = true;
+	}
+});
+
+API.onUpdate.connect(function(sender, events) {
+	menuPool.ProcessMenus();
+});
---
+++ spawner/Server/Main.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using GrandTheftMultiplayer.Server.API;
+using GrandTheftMultiplayer.Shared;
+using GrandTheftMultiplayer.Server.Elements;
+using GrandTheftMultiplayer.Server.Managers;
+using GrandTheftMultiplayer.Shared.Math;
+
+public class Spawner : Script
+{
+    private Dictionary<Client, NetHandle> _vehicleHistory = new Dictionary<Client, NetHandle>();
+    
+    public Spawner()
+    {
+        API.onClientEventTrigger += onClientEventTrigger;
+    }
+
+    public void onClientEventTrigger(Client sender, string name, object[] args)
+    {
+        if (name != "CREATE_VEHICLE") return;
+
+        var model = (int)args[0];
+
+        if (!Enum.IsDefined(typeof(VehicleHash), model)) return;
+
+        var rot = API.getEntityRotation(sender.handle);
+        var veh = API.createVehicle((VehicleHash)model, sender.position, new Vector3(0, 0, rot.Z), 0, 0);
+
+        if (_vehicleHistory.ContainsKey(sender) && _vehicleHistory[sender] != null && API.doesEntityExist(_vehicleHistory[sender]))
+        {
+            API.deleteEntity(_vehicleHistory[sender]);
+        }
+
+        _vehicleHistory[sender] = veh;
+
+        API.setPlayerIntoVehicle(sender, veh, -1);     
+    }
+}
